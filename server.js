aefe9957d7d62934feaf65c6211fe4ba066bcf24
Dane Edwards <Daneedw@gmail.com>
2021-10-10 23:04:50 -0500

    eslint and CI

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const express = require("express")
+const express = require('express');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
-
-
-
-app.listen(PORT,() =>console.log(`server running on port ${PORT}!`))
-
-
+app.listen(PORT, () => console.log(`server running on port ${PORT}!`));
